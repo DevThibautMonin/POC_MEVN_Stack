6d922c02a72cf7f5fbf71602f7788ac902bcb451
Thibaut Monin <device.thibaut.monin@gmail.com>
2022-10-12 22:14:43 +0200

    Création de l'index du serveur avec les configs

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
 import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import todoRouter from "./routes/todoRouter.js"
+import config from "config"
 
-const PORT = 3000
+const PORT = config.get("server.port")
+const mongoBaseUrl = config.get("server.mongoBaseUrl")
+
 const app = express()
+app.use(express.json())
+app.use(cors())
+app.use("/todolist", todoRouter)
 
-app.get("/", (req, res) =>{
-  res.send("Hello world.")
-} )
+mongoose.connect(mongoBaseUrl, {
+  useNewUrlParser : true,
+  useUnifiedTopology : true
+}).then(
+  () => console.log("MongoDB database connected.")
+).catch((err) => {
+  console.log(err)
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
